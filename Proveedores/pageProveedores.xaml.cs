c459154a134e9fc99ef9dfe3156d45efc098a4d9
albl2008 <alvaroblanco10@gmail.com>
2017-09-27 13:03:55 -0300

    Proveedores

--- Proveedores/pageProveedores.xaml.cs
+++ Proveedores/pageProveedores.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,21 +23,69 @@ namespace wpfFamiliaBlanco
     /// </summary>
     public partial class pageProveedores : Page
     {
+
+        CRUD conexion = new CRUD();
+
         public pageProveedores()
         {
             InitializeComponent();
+            loadListaProveedores();
+
         }
 
         private void btnModificar_Click(object sender, RoutedEventArgs e) //btnModificarProveedor_Click
         {
             var newW = new windowModificarProveedor();
-            newW.Show();
+            newW.ShowDialog();
         }
 
         private void btnAgregar_Click(object sender, RoutedEventArgs e)
         {
             var newW = new windowAgregarProveedor();
-            newW.Show();
+            newW.ShowDialog();
+        }
+
+        
+        private void loadListaProveedores()
+        {
+            String consulta = "SELECT * FROM Proveedor";
+            conexion.Consulta(consulta, ltsProveedores);
+            ltsProveedores.DisplayMemberPath = "nombre";
+            ltsProveedores.SelectedValuePath = "idProveedor";
+
+        }
+
+        private void ltsProveedores_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+
+                //consulta datosproveedor
+                String consulta = "SELECT * from proveedor WHERE idProveedor = @valor";
+                DataTable proveedor = conexion.ConsultaParametrizada(consulta, ltsProveedores.SelectedValue);
+                txtCuit.Text = proveedor.Rows[0].ItemArray[3].ToString();
+                txtDireccion.Text = proveedor.Rows[0].ItemArray[5].ToString();
+                txtRazonSocial.Text = proveedor.Rows[0].ItemArray[2].ToString();
+                txtLocalidad.Text = proveedor.Rows[0].ItemArray[6].ToString();
+                txtCP.Text = proveedor.Rows[0].ItemArray[4].ToString();
+
+                //consulta contacto
+                String consultaContacto = "SELECT contactoproveedor.telefono, contactoproveedor.email, contactoproveedor.nombreContacto from contactoproveedor WHERE FK_idProveedor=@valor";
+                DataTable contacto = conexion.ConsultaParametrizada(consultaContacto, ltsProveedores.SelectedValue);
+                dgvContacto.ItemsSource = contacto.AsDataView();
+
+                //consulta categorias
+                String consultaCategoria = "SELECT categorias.nombre from categorias , categorias_has_proveedor WHERE categorias_has_proveedor.FK_idProveedor = @valor AND categorias_has_proveedor.FK_idCategorias=categorias.idCategorias";
+                DataTable categorias = conexion.ConsultaParametrizada(consultaCategoria, ltsProveedores.SelectedValue);
+                ltsCategorias.ItemsSource = categorias.AsDataView();
+                ltsCategorias.DisplayMemberPath = "nombre";
+
+
+            }
+            catch
+            {
+
+            }
         }
     }
 }
