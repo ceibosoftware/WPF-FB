848407b7b34798fb5d595d0ccd376565dc077c51
Mariano <marianobuglio@gmail.com>
2017-12-11 16:15:09 -0300

    Dinamica pantalla Agregar OC

--- Entradas/windowAgregarOC.xaml.cs
+++ Entradas/windowAgregarOC.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,15 +21,18 @@ namespace wpfFamiliaBlanco.Entradas
     public partial class windowAgregarOC : Window
     {
 
-
+        List<producto> productos = new List<producto>() ;
+        decimal subtotal;
+        decimal total;
         CRUD conexion = new CRUD();
         public windowAgregarOC()
         {
             InitializeComponent();
-
+            loadDgvProductos();
             LoadListaComboProveedor();
             LlenarCmbIVA();
             LlenarCmbTipoCambio();
+            loadCmbfiltro();
                   
         }
 
@@ -65,8 +69,23 @@ namespace wpfFamiliaBlanco.Entradas
 
         private void btnAgregar_Copy_Click(object sender, RoutedEventArgs e)
         {
-            var newW = new windowAgregarClienteME();
+            var newW = new windowAgregarClienteME((int)cmbProveedores.SelectedValue);
             newW.ShowDialog();
+           
+            if (newW.DialogResult == true)
+            {
+                int.TryParse(newW.txtCantidad.Text, out int cantidad);
+                decimal.TryParse(newW.txtTotal.Text, out decimal total);
+                decimal.TryParse(newW.txtPrecioUnitario.Text, out decimal precioU);
+                producto p = new producto(newW.idProducto, newW.txtNombre.Text, cantidad,precioU,total);
+                productos.Add(p);
+                loadDgvProductos();
+                dgvProductos.Items.Refresh();
+                decimal.TryParse(txtSubtotal.Text, out  subtotal);
+                subtotal += p.total;
+                txtSubtotal.Text = (subtotal).ToString() ;
+                calculaTotal();
+            }
         }
 
 
@@ -81,16 +100,123 @@ namespace wpfFamiliaBlanco.Entradas
 
         private void LlenarCmbIVA()
         {
-            cmbIVA_Copy.Items.Add("0");
-            cmbIVA_Copy.Items.Add("21");
-            cmbIVA_Copy.Items.Add("10,5");
+            cmbIVA.Items.Add((decimal)0);
+            cmbIVA.Items.Add((decimal)21);
+            cmbIVA.Items.Add((decimal)10.5);
+            
         }
 
         private void LlenarCmbTipoCambio()
         {
-            cmbTipoCambio_Copy.Items.Add("$");
-            cmbTipoCambio_Copy.Items.Add("u$d");
-            cmbTipoCambio_Copy.Items.Add("€");
+            cmbTipoCambio.Items.Add("$");
+            cmbTipoCambio.Items.Add("u$d");
+            cmbTipoCambio.Items.Add("€");
+        }
+        private void loadDgvProductos()
+        {
+           
+            dgvProductos.ItemsSource = productos;
+           
+            
+        }
+
+        private void cmbIVA_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            calculaTotal();
+              
+        }
+        public void calculaTotal()
+        {
+            if (cmbIVA.SelectedIndex == 0)
+            {
+                txtTotal.Text = subtotal.ToString();
+            }
+            else if (cmbIVA.SelectedIndex == 1)
+            {
+                total = subtotal * (decimal)1.21;
+                txtTotal.Text = total.ToString();
+            }
+            else if (cmbIVA.SelectedIndex == 2)
+            {
+                total = subtotal * (decimal)1.105;
+                txtTotal.Text = total.ToString();
+            }
+        }
+
+        private void txtFiltro_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Busquedas de productos.
+            DataTable productos = new DataTable();
+            String consulta;
+
+            if (cmbFiltro.Text == "Nombre")
+            {   //Busca por nombre
+                consulta = "SELECT * FROM proveedor WHERE proveedor.nombre LIKE '%' @valor '%'";
+                productos = conexion.ConsultaParametrizada(consulta, txtFiltro.Text);
+                
+            }
+            else if (cmbFiltro.Text == "Categoria" )
+            {
+                //busca por nombre de categoria (posibilidad de agregar combobox)
+               consulta = "SELECT proveedor.nombre ,categorias.idCategorias FROM categorias , proveedor, categorias_has_proveedor WHERE  categorias.idCategorias = categorias_has_proveedor.FK_idCategorias and categorias.nombre LIKE '%' @valor '%'";
+               productos = conexion.ConsultaParametrizada(consulta, txtFiltro.Text);
+              
+            }
+            
+
+
+           cmbProveedores.ItemsSource = productos.AsDataView();
+            cmbProveedores.SelectedIndex = 0;
+        }
+
+        public void loadCmbfiltro()
+        {
+            cmbFiltro.Items.Add("Nombre");
+            cmbFiltro.Items.Add("Categoria");
+        }
+
+        private void btnEliminar_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            producto prod = dgvProductos.SelectedItem as producto;
+            subtotal -= prod.total;
+            calculaTotal();
+            txtSubtotal.Text = (subtotal).ToString();
+            productos.Remove(prod);
+            dgvProductos.Items.Refresh();
+        }
+
+        private void btnModificar_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            producto prod = dgvProductos.SelectedItem as producto;
+            decimal.TryParse(txtSubtotal.Text, out subtotal);
+            subtotal -= prod.total;
+            var newW = new windowAgregarClienteME((int)cmbProveedores.SelectedValue, prod.id);
+            newW.txtCantidad.Text = prod.cantidad.ToString();
+            newW.txtPrecioUnitario.Text = prod.precioUnitario.ToString();
+            newW.ShowDialog();
+        
+
+            if (newW.DialogResult == true)
+            {
+               int.TryParse(newW.txtCantidad.Text, out int cantidad);                
+               decimal.TryParse(newW.txtTotal.Text, out decimal total);
+               decimal.TryParse(newW.txtPrecioUnitario.Text, out decimal precioU);
+                prod.cantidad = cantidad;
+                prod.total = total;
+                prod.precioUnitario = precioU;
+                prod.nombre = newW.txtNombre.Text;
+                prod.id = newW.idProducto;              
+                dgvProductos.Items.Refresh();               
+                subtotal += prod.total;
+                txtSubtotal.Text = (subtotal).ToString();
+                calculaTotal();
+            }
+        }
+
+        private void cmbProveedores_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
         }
     }
 }
--- Entradas/windowAgregarProcductoOC.xaml.cs
+++ Entradas/windowAgregarProcductoOC.xaml.cs
@@ -20,32 +20,56 @@ namespace wpfFamiliaBlanco.Entradas
     /// </summary>
     public partial class windowAgregarClienteME : Window
     {
-       
+        public int idProducto;
         CRUD conexion = new CRUD();
-        public windowAgregarClienteME()
+        public windowAgregarClienteME(int idProveedor)
         {
             InitializeComponent();
-            loadListaProducto();
-            
+            loadListaProducto(idProveedor);
+
+        }
+        public windowAgregarClienteME(int idProveedor, int idProducto)
+        {
+            InitializeComponent();
+            loadListaProducto(idProveedor, idProducto);
+
         }
 
-        public void loadListaProducto()
+        public void loadListaProducto(int idProveedor)
         {
-            String consulta = " Select * from productos ";
+            String consulta = " Select p.nombre , p.idProductos from productos p inner join productos_has_proveedor t2 where t2.FK_idProveedor =" + idProveedor.ToString() + " and p.idProductos = t2.FK_idProductos";
             conexion.Consulta(consulta, ltsProductos);
             ltsProductos.DisplayMemberPath = "nombre";
             ltsProductos.SelectedValuePath = "idProductos";
+            ltsProductos.SelectedIndex = 0;
 
         }
+        public void loadListaProducto(int idProveedor, int idProducto)
+        {
+            String consulta = " Select p.nombre , p.idProductos from productos p inner join productos_has_proveedor t2 where t2.FK_idProveedor =" + idProveedor.ToString() + " and p.idProductos = t2.FK_idProductos";
+            conexion.Consulta(consulta, ltsProductos);
+            ltsProductos.DisplayMemberPath = "nombre";
+            ltsProductos.SelectedValuePath = "idProductos";
+            ltsProductos.SelectedIndex = 0;
+            ltsProductos.SelectedValue = idProducto;
+            
+        }
+
+   
+
+
 
         private void ltsProductos_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
-            String consulta = "SELECT p.nombre , p.precioUnitario from productos p where p.idProductos  = @valor";
+           
+            String consulta = "SELECT p.nombre , p.precioUnitario , p.idProductos from productos p where p.idProductos  = @valor";
+           
+            Console.WriteLine(ltsProductos.SelectedValue.ToString());
             DataTable productos = conexion.ConsultaParametrizada(consulta, ltsProductos.SelectedValue);
+                    
             txtNombre.Text = productos.Rows[0].ItemArray[0].ToString();
             txtPrecioUnitario.Text = productos.Rows[0].ItemArray[1].ToString();
-
+            idProducto = (int)productos.Rows[0].ItemArray[2];
             if (txtCantidad.Text != "")
             {
                 txtTotal.Text = 0.ToString();
@@ -66,5 +90,46 @@ namespace wpfFamiliaBlanco.Entradas
             txtTotal.Text = (cantidad * precioU).ToString();
 
         }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            if (validar())
+            {
+                DialogResult = true;
+            }
+            
+        }
+
+        public bool validar()
+        {
+
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                MessageBox.Show("Falta completar campo nombre");
+                return false;
+            }
+            else if (string.IsNullOrEmpty(txtCantidad.Text))
+            {
+                MessageBox.Show("falta completar campo cantidad");
+                return false;
+            }
+            else if (string.IsNullOrEmpty(txtPrecioUnitario.Text))
+            {
+                MessageBox.Show("falta completar precio unitario");
+                return false;
+            }
+           
+            else if (string.IsNullOrEmpty(txtTotal.Text))
+            {
+                MessageBox.Show("falta completar campo total");
+                return false;
+            }          
+            else
+            {
+                return true;
+            }
+
+        }
+
     }
 }
