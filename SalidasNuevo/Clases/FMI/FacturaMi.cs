795112b06a0dfceb26e227bf99aac29f56e4a117
Your Name <nicolas@ceibosoftware.com>
2018-08-21 10:12:44 -0300

    se crea carpeta de FMI

---
+++ SalidasNuevo/Clases/FMI/FacturaMi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfFamiliaBlanco.SalidasNuevo.Clases.FMI
+{
+    class FacturaMi
+    {
+
+    }
+}
