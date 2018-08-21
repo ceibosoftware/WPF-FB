2cecb11da510b936f7d3231f7f10b06b390fe419
marianobuglio <marianobuglio@gmail.com>
2018-08-21 17:21:24 -0300

    agrego pantallas agregar pallet y agregar producto factura proforma

---
+++ SalidasNuevo/Vistas/FacturaProforma/windowAgregarPallet.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfFamiliaBlanco.SalidasNuevo.Vistas.FacturaProforma
+{
+    /// <summary>
+    /// Interaction logic for windowAgregarPallet.xaml
+    /// </summary>
+    public partial class windowAgregarPallet : Window
+    {
+        public windowAgregarPallet()
+        {
+            InitializeComponent();
+        }
+    }
+}
