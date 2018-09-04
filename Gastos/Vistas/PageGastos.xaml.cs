fcc5de2fe1382e84a52e0573900479149b52d447
nicoschmidt12 <nicolas-schmidt@hotmail.com>
2018-09-04 10:37:30 -0300

    Se agrega clases y vistas de gastos

---
+++ Gastos/Vistas/PageGastos.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfFamiliaBlanco.Gastos.Vistas
+{
+    /// <summary>
+    /// Interaction logic for PageGastos.xaml
+    /// </summary>
+    public partial class PageGastos : Page
+    {
+        public PageGastos()
+        {
+            InitializeComponent();
+        }
+    }
+}
